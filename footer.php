225ae8e1d14cc86265fcabfd2a9528cbd1f81fb7
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-01-02 21:30:51 +0100

    Ajout du footer commun à toutes les pages

---
+++ footer.php
@@ -0,0 +1,51 @@
+<footer id="footer">
+	<div id="footer-top">
+	 <ul>
+	  <li><div class="titre-footer">PORT<b>FOLIO</b></div></li>
+	  <li><p>Emmanuelle Husson, née le 12/04/1995 est née passionnée par l'Art, la création et le Design. Elle pense que l'humain ne doit créer que s'il arrive à hauteur de la nature en terme d'esthétisme. S'il n'y arrive pas, alors il faut laisser tomber. La nature n'a pas besoin de lui, elle se créée seule. De plus il faut que la création n'ait pas d'impact négatif. Les dommages sont collosaux mais elle garde espoir.</p></li>
+	 </ul>
+		
+	 <ul>
+	  <li><div class="titre-footer">Skills</div></li>
+	  <li><p>Design<br/><a href="#">UIUX</a><br/>Analytic Drawing<br/>Art Direction<br/>Code</p></li>
+	  <li><p>Has been in Crête, Grèce, Ecosse, Maroc, Stockhlom, Berlin, Amsterdam, Espagne, Tunisie, Sicile, Malte, Corse, Chypre, Londres...</p></li>
+	 </ul>	
+		
+	 <ul>
+	  <li><div class="titre-footer">Follow</div></li>
+	
+	  <li id="img_footer">
+	   <a href="#"><img src="images/icon-facebook-footer.png"></a>
+	   <a href="#"><img src="images/icon-twitter-footer.png"></a>
+	   <a href="https://www.youtube.com/channel/UCHQyXitCjlQQrSXQCB-fn0w?"><img src="images/icon-youtube-footer.png"></a>
+	   <a href="https://www.behance.net/hussonemmad23e"><img src="images/icon-behance-footer.png"></a>
+	   <a><img id="logo_footer" src="images/logo.png"></a>
+	  </li>
+	 </ul>
+		
+<div class="clearboth"></div>		
+	</div>
+
+</footer>
+	
+<footer id="footer2">
+	<div id="footer-bottom">
+		<ul class="copyright">
+			<li>©<b>HEMASN</b> | Digital Designer</li>
+		</ul>
+		
+		<ul class="liens">
+			<li><a href="#">Hello</a> | </li>
+			<li><a href="#">About</a> | </li>
+			<li><a href="#">Portfolio</a> | </li>
+			<li><a href="#">Contact</a> | </li>
+			<li><a href="#">Mentions légales</a> | </li>
+			<li><a href="#">Map</a>  </li>
+		</ul>
+		
+	</div>
+</footer>
+
+
+</body>
+</html>
