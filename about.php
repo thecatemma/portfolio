2925650dc4bf32f77e93ae13bcdfddaa9f037f9c
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-01-02 21:27:14 +0100

    Ajout de la page About

---
+++ about.php
@@ -0,0 +1,54 @@
+<?php		
+	include 'header.php';
+
+?>		
+		<div id="logo"><a href="#">HE<b>MASN</b>.</a></div>
+		
+		<nav id="menu">
+			<ul>
+				<li><a href="index.php" title="Index">Hello</a></li>
+				<li><a class="active" href="about.php" title="About">About</a></li>
+				<li><a href="portfolio.php" title="Portfolio">Porte folie</a></li>
+				<li><a href="blog.php" title="Blog">-Mia ? -Miaowww</a></li>
+				<li><a href="contact.php" title="Contact">Keep in touch</a></li>
+			</ul>
+		</nav>
+		
+		<div class="clearboth"></div>
+		
+		<section id="contenu-accueil">
+			<article>
+				<h1><span class="jaune">ABOUT</span>ME</h1>
+			</article>
+		</section>
+		
+	</header>
+
+<section id="CV">
+
+<h1>FORMATIONS</h1>
+
+<h3><b>2018/2019</b><br>First year Master Degree - Digital Creation UIUX<br>
+    2018<br>BTS Web Development<br>
+    2016/2017<br>Numeric<br>
+	2014/2015<br>Graphic Design<br>
+	2013/2014<br>Foundation Year in Applied Arts<br>
+</h3>
+
+<h1>TOOLS</h1>
+
+<h3>Photoshop<br>Illustrator<br>InDesign<br>After Effect<br>Sketch<br>Adobe XD<br>Premiere<br>HTML<br>CSS<br></h3>
+
+<h1>EXPERIENCE</h1>
+<h3>Two internships<br>Free-lance</h3>
+
+<h1>INTERESTS</h1>
+<h3>Travels <br> Environment <br> Nature</h3>
+
+<h1>LANGUAGES</h1>
+<h3>French<br>English</h3>
+</div>
+
+<?php		
+	include 'footer.php';
+?>
