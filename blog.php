6ba8aff04b85a396fd9472f53c439cba650be586
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-01-02 21:28:18 +0100

    Ajout de la page blog.php

---
+++ blog.php
@@ -0,0 +1,52 @@
+<?php		
+	include 'header.php';
+
+?>
+		<div id="logo"><a href="#">HE<b>MASN</b>.</a></div>
+		
+		<nav id="menu">
+			<ul>
+				<li><a href="index.php" title="">Hello</a></li>
+				<li><a href="about.php" title="">About</a></li>
+				<li><a href="portfolio.php" title="">Porte folie</a></li>
+				<li><a class="active" href="blog.php" title="">-Mia ? -Miaowww</a></li>
+				<li><a href="contact.php" title="">Keep in touch</a></li>
+			</ul>
+		</nav>
+		
+		<div class="clearboth"></div>
+		
+		<section id="contenu-accueil">
+			<article>
+				<h1><span class="jaune">A</span>PROPOS</h1>
+			</article>
+		</section>
+		
+	</header>
+	
+
+
+<article class="videos">
+
+<ul>
+	
+<li id="video2">
+<h3>FUNK THAT</h3>
+<iframe width="450" height="250" src="https://www.youtube.com/embed/C9dP2YIgNRI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</li>
+
+<li id="video2">
+<h3>CHANNEL</h3>
+<iframe width="450" height="250" src="https://www.youtube.com/embed/1LvL__JC8Yw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</li>
+	
+</ul>
+
+</section>
+	
+
+</div>	
+
+<?php		
+	include 'footer.php';
+?>
