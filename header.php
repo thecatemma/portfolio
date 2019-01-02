596a204fd84957336ea534acd64a3f28388e9a49
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-01-02 21:30:30 +0100

    Ajout du header commun à toutes les pages

---
+++ header.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>HEMASN : Mon Portfolio</title>
+	<link rel="icon" type="images/png" href="images/favicon.ico">
+<!-- Autres pages -->
+	<link rel="stylesheet" type="text/css" about.php/>
+	<link rel="stylesheet" type="text/css" portfolio.php/>
+	<link rel="stylesheet" type="text/css" blog.php/>
+	<link rel="stylesheet" type="text/css" contact.php/>
+<!-- CSS  -->
+	<link rel="stylesheet" href="style.css" type="text/css" /><!-- Appel de la feuille de stlyle générique -->
+	<link rel="stylesheet" href="style/normalize.css" type="text/css"><!-- réinitialisation des valeurs à 0 sur des propriétés CSS  -->
+    <link rel="stylesheet" type="text/css" href="style/jquery.bxslider.css" /><!-- Appel du carousel clients / projets -->
+<!-- Bootstrap  -->
+	<link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" />
+<!-- JS -->
+	<script src="js/modernizr.custom.js"></script><!-- Bibliothèque JavaScript conçue pour détecter des fonctionnalités spécifiques de HTML et CSS dans les navigateurs -->
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.js"></script><!-- Librairie JQUERY -->
+	<script src="js/jquery.bxslider.min.js"></script><!-- Appel JS du carousel -->
+    <script type="text/javascript" src="js/jquery.isotope.min.js"></script><!-- Appel JS du Portfolio -->
+<!-- Appel Google fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,500,700,800' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC:400,700,900" rel="stylesheet type='text/css">
+	<link href="https://fonts.googleapis.com/css?family=Pattaya" rel="stylesheet">
+<!-- Autorise l'usage des nouveaux éléments HTML5 sur IE <9 -->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+    <meta name="viewport" content="width=device-width" /><!-- Optimisation en largeur pour correspondre à la largeur de l'écran du périphérique -->
+</head>
+
+<body>
+<div id="header"> </div>
+<div id="conteneur">
+	<header id="header-accueil">
+		<div id="header-top">
+			<div id="recherche">
+				<div id="wrap">
+				<!--  <form action="" autocomplete="on">
+  				 <input id="search" name="search" type="text" placeholder="Vous recherchez ?"><input id="search_submit" value="Rechercher" type="submit">
+                 </form> -->
+                </div>
+			</div>
+			
+			<div id="social">
+				<a href="#" title="facebook"><img src="images/header-facebook.png" alt="Mon facebook"></a>
+				<a href="https://twitter.com/EmmaSalander" title="Twitter"><img src="images/header-twitter.png" alt="Mon Twitter"></a>
+				<a id="behance" href="https://www.behance.net/hussonemmad23e" title="Google +"><img src="images/header-behance.png" alt="Behance"></a>
+			</div>
+		</div> 
+  
