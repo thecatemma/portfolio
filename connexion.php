0b445cf9e7f52b8f3b569a08d9859ba13dafa83e
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-01-04 18:12:18 +0100

    optimisation de la page avec MDP

--- connexion.php
+++ connexion.php
@@ -4,8 +4,9 @@
 include('./header.php'); //contient le doctype, et head.
 
 ?>
-<h2>Mot de passe :</h2>
+
 	 <form action="secret.php" method="post">
+		 <h2>Mot de passe :</h2>
             <p>
             <input type="password" name="mot_de_passe" />
             <input type="submit" value="Valider" />
