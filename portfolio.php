3506a257cac34959b72450623e7bab053cf3e9ed
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-01-21 23:28:45 +0100

    Bootstrap + qqch pour afficher portfolio

--- portfolio.php
+++ portfolio.php
@@ -27,10 +27,130 @@
 		
 	</header>
 
-	
-<div class="clearboth"></div>
-
 </div>	
+
+
+<body class="bg--blue">
+  <a href="https://github.com/jh3y/driveway"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 9999;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
+  <div class="dw">
+    <div class="dw-pnl dw-pnl--fcs">
+      <div class="dw-pnl__cntnt bd--white tx--white">
+        <h1>Pure CSS masonry layout</h1>
+      </div>
+    </div>
+    <div class="dw-pnl">
+      <div class="dw-pnl__cntnt bd--white tx--white">
+        <ul>
+          <li>Configurable</li>
+          <li>Responsive</li>
+          <li>Supports clusters</li>
+        </ul>
+      </div>
+    </div>
+    <div class="dw-pnl "><img src="https://unsplash.it/419/?random" class="dw-pnl__cntnt"/>
+    </div>
+    <div class="dw-pnl ">
+      <div class="dw-pnl__cntnt tx--white bd--white">
+        <p>Phasellus malesuada, urna non auctor viverra, libero ex pellentesque urna, id rhoncus eros lacus tristique risus. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+      </div>
+    </div>
+    <div class="dw-pnl ">
+      <img src="https://unsplash.it/445/?random" class="dw-pnl__cntnt"/>
+    </div>
+    <div class="dw-pnl ">
+      <img src="https://unsplash.it/423/?random" class="dw-pnl__cntnt"/>
+    </div>
+    <div class="dw-pnl dw-clstr dw-clstr--hrz">
+      <div class="dw-clstr__sgmnt dw-clstr__sgmnt--rw ">
+        <div class="dw-pnl dw-clstr__sgmnt ">
+          <div class="dw-pnl__cntnt bd--white tx--white">
+            <h2>How</h2>
+          </div>
+        </div>
+      </div>
+      <div class="dw-clstr__sgmnt dw-clstr__sgmnt--rw ">
+        <div class="dw-pnl dw-clstr__sgmnt ">
+          <div class="dw-pnl__cntnt bd--white tx--white">
+            <h2>about</h2>
+          </div>
+        </div>
+        <div class="dw-pnl dw-clstr__sgmnt ">
+          <div class="dw-pnl__cntnt bd--white tx--white">
+            <h2>clusters?</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="dw-pnl ">
+      <img src="https://unsplash.it/420/?random" class="dw-pnl__cntnt"/>
+    </div>
+    <div style="height: 200px;" class="dw-pnl dw-flp">
+      <div class="dw-pnl__cntnt dw-flp__cntnt">
+        <div class="dw-flp__pnl dw-flp__pnl--frnt tx--white bd--white tx--center">
+          <h1>You can flip me round</h1>
+        </div>
+        <div class="dw-flp__pnl dw-flp__pnl--bck bd--white tx--white tx--center">
+          <h1>Yeah that's right</h1>
+        </div>
+      </div>
+    </div>
+    <div class="dw-pnl dw-clstr dw-clstr--vrt">
+      <div class="dw-clstr__sgmnt dw-clstr__sgmnt--clmn ">
+        <div class="dw-pnl dw-clstr__sgmnt ">
+          <div class="dw-pnl__cntnt bd--white tx--white">
+            <h2>A</h2>
+          </div>
+        </div>
+      </div>
+      <div class="dw-clstr__sgmnt dw-clstr__sgmnt--clmn ">
+        <div class="dw-pnl dw-clstr__sgmnt ">
+          <div class="dw-pnl__cntnt bd--white tx--white">
+            <h2>vertically</h2>
+          </div>
+        </div>
+        <div class="dw-pnl dw-clstr__sgmnt ">
+          <div class="dw-pnl__cntnt bd--white tx--white">
+            <h2>flowed</h2>
+          </div>
+        </div>
+        <div class="dw-pnl dw-clstr__sgmnt ">
+          <div class="dw-pnl__cntnt bd--white tx--white">
+            <h2>cluster</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="dw-pnl "><img src="https://unsplash.it/424/?random" class="dw-pnl__cntnt"/>
+    </div>
+    <div class="dw-pnl ">
+      <div class="dw-pnl__cntnt bd--white bg--darkred tx--white">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu justo ex. Praesent mollis augue sagittis eros pharetra feugiat. Phasellus dignissim est lacus. Sed nec imperdiet dolor, sit amet mattis ex. Sed sed augue eu neque tristique commodo. Mauris aliquet tortor sollicitudin nibh molestie, id egestas nisl sollicitudin. Aliquam erat volutpat. Donec quis ultrices ligula. Cras sed purus risus. Curabitur quis eros eu tortor semper eleifend. Pellentesque lorem elit, dignissim interdum massa id, malesuada rutrum ligula. Suspendisse tempor quis mauris eu facilisis. Phasellus non volutpat diam, non dapibus ligula. Ut non molestie ex, nec sagittis mi. Curabitur suscipit tellus id dolor pretium blandit. Cras tristique tristique pharetra.</p>
+      </div>
+    </div>
+    <div class="dw-pnl "><img src="https://unsplash.it/425/?random" class="dw-pnl__cntnt"/>
+    </div>
+    <div class="dw-pnl dw-pnl--fcs">
+      <div class="dw-pnl__cntnt tx--white bd--white tx--center">
+        <h1>Focus on hover</h1>
+      </div>
+    </div>
+    <div class="dw-pnl ">
+      <div class="dw-pnl__cntnt tx--white bd--white">
+        <h2>Title for some content</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu justo ex. Praesent mollis augue sagittis eros pharetra feugiat. Phasellus dignissim est lacus. Sed nec imperdiet dolor, sit amet mattis ex. Sed sed augue eu neque tristique commodo. Mauris aliquet tortor sollicitudin nibh molestie, id egestas nisl sollicitudin. Aliquam erat volutpat. Donec quis ultrices ligula. Cras sed purus risus. Curabitur quis eros eu tortor semper eleifend.</p>
+      </div>
+    </div>
+    <div class="dw-pnl dw-pnl--pls">
+      <div class="dw-pnl__cntnt tx--white bd--white bg--darkred tx--center">
+        <h1>Pulse on hover</h1>
+      </div>
+    </div>
+    <div class="dw-pnl "><img src="https://unsplash.it/440/?random" class="dw-pnl__cntnt"/>
+    </div>
+    <div class="dw__fcs-crtn"></div>
+  </div>
+</body>
+
 
 <div id="conteneur">
 
