56e232be8d167a50cf0b3f5416f1b5b77be4630c
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-06-20 18:00:35 +0200

    modification html + regarder css

--- about.php
+++ about.php
@@ -24,68 +24,47 @@
 		
 	</header>
 
+	<section id="CV">
 
-	<body class="demo-2 loading">
-		<main>
-			<div class="message">Please view on desktop to see the effect</div>
+	<h1>FORMATIONS </h1>
+	<h3><b>2019/2020</b><br>Second year Master Degree - UX Design<br>
+		2018/2019<br>First year Master Degree - Digital Creation UIUX<br>
+		2018/2019<br>Formation Web Development +2<br>
+		2016/2017<br>Numeric Year (UX, UI, Code, Management) +1<br>
+		2014/2015<br>Graphic Design +1<br>
+		2013/2014<br>Foundation Year in Applied Arts MANAA<br>
+	</h3>
 
-			<div class="content">
-				<div class="content__img-wrap">
-					<div class="content__img"></div>
-					<div class="content__img"></div>
-					<div class="content__img"></div>
-				</div>
-				<div class="content__text"><span class="content__text-inner">.011</span></div>
-			</div>
-		</main>
-		<script src="js2/blotter.min.js"></script>
-        <script src="js2/materials/liquidDistortMaterial.js"></script>
-		<script src="js2/imagesloaded.pkgd.min.js"></script>
-		<script src="js2/demo2.js"></script>
+	<h1>TOOLS</h1>
 
-<section id="CV">
-
-<h1>FORMATIONS </h1>
-<h3><b>2019/2020</b><br>Second year Master Degree - UX Design<br>
-	2018/2019<br>First year Master Degree - Digital Creation UIUX<br>
-    2018/2019<br>Formation Web Development +2<br>
-    2016/2017<br>Numeric Year (UX, UI, Code, Management) +1<br>
-	2014/2015<br>Graphic Design +1<br>
-	2013/2014<br>Foundation Year in Applied Arts MANAA<br>
-</h3>
-
-<h1>TOOLS</h1>
-	
-<table>
-	<tr>
-		<th><h2>Programs</h2></th>
-		<td><h3>Photoshop</h3></td>
-	    <td><h3>Illustrator</h3></td>
-		<td><h3>InDesign</h3></td>
-		<td><h3>After Effect</h3></td>
-		<td><h3>Premiere</h3></td>
-	</tr>
-	<tr>
-		<th><h2>Sketching</h2></th>
-		<td><h3>Adobe XD</h3></td>
-	    <td><h3>Sketch</h3></td>
-		<td><h3>Figma</h3></td>
-		<td><h3>Principle</h3></td>
-		<td><h3>Zeplin</h3></td>
-	</tr>
+	<table>
 		<tr>
-		<th><h2>Coding</h2></th>
-		<td><h3>html</h3></td>
-	    <td><h3>css</h3></td>
-		<td><h3>js</h3></td>
-		<td><h3>php</h3></td>
-		<td><h3>mysql</h3></td>
-	</tr>
+			<th><h2>Programs</h2></th>
+			<td><h3>Photoshop</h3></td>
+			<td><h3>Illustrator</h3></td>
+			<td><h3>InDesign</h3></td>
+			<td><h3>After Effect</h3></td>
+			<td><h3>Premiere</h3></td>
+		</tr>
+		<tr>
+			<th><h2>Sketching</h2></th>
+			<td><h3>Adobe XD</h3></td>
+			<td><h3>Sketch</h3></td>
+			<td><h3>Figma</h3></td>
+			<td><h3>Principle</h3></td>
+			<td><h3>Zeplin</h3></td>
+		</tr>
+			<tr>
+			<th><h2>Coding</h2></th>
+			<td><h3>html</h3></td>
+			<td><h3>css</h3></td>
+			<td><h3>js</h3></td>
+			<td><h3>php</h3></td>
+			<td><h3>mysql</h3></td>
+		</tr>
 
-</table>
-<br>
+	</table><br>
 	
-
 <h1>EXPERIENCE</h1>
 <h3>Three internships<br>
 Free-lance<br>Projects<br>Life<br></h3>
--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 	 <ul>
 	  <li><div class="titre-footer">Skills</div></li>
 	  <li><p>UIUX<br/>Design<br/>Analytic Drawing<br/>Art Direction<br/>Code</p></li>
-	  <li><p>Crête, Grèce, Ecosse, Maroc, Stockhlom, Berlin, Amsterdam, Espagne, Tunisie, Sicile, Malte, Corse, Chypre, Londres, Prague... <br/>and France of course</p></li>
+	  <li><p>Crête, Grèce, Ecosse, Maroc, Stockhlom, Berlin, Amsterdam, Espagne, Tunisie, Sicile, Malte, Corse, Chypre, Londres, Prague... and France of course</p></li>
 	 </ul>	
 		
 	 <ul>
