afc6efc3c8c5ec95657a9f020024021b5e0c204a
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-06-20 15:50:05 +0200

    Test nouvel élément avec fichiers Js

--- header.php
+++ header.php
@@ -33,6 +33,10 @@
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <!-- Optimisation en largeur pour correspondre à la largeur de l'écran du périphérique -->
     <meta name="viewport" content="width=device-width" />
+	
+	
+	
+	<script>document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");</script>
 </head>
 
 <body>
