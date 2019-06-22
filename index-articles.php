e309a1ca84d08c5bedc93140e91d9713b5b5842c
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-06-22 21:21:42 +0200

    commenter chaque partie de code obscure

--- index-articles.php
+++ index-articles.php
@@ -3,6 +3,8 @@
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
+//error_reporting() modifie la directive error_reporting pendant l'exécution du script. PHP possède plusieurs niveaux d'erreurs, utiliser cette fonction configure ce niveau pendant la durée (d'exécution) de votre script. 
+
 require_once('./functions.php');
 $articles = getArticles();
 
@@ -13,6 +15,8 @@ $articles = getArticles();
 			<?php foreach($articles as $article): ?>
 			<?php /*  print_r($article); */ ?>
 				<h3><?= $article->title ?></h2>
+			
+			
 				<a id="buttoninfo" href="article.php?id=<?= $article->id ?>" class="btn btn-primary"> Lire l'article</a>
 				<br/><br/>_______ <br/>
 			<?php endforeach; ?>
