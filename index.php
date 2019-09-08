170bc9406de53926c56b2ad30f320e31eea80a10
thecatemma <husson.emmanuelle.miou@gmail.com>
2019-09-08 23:23:00 +0200

    slider

--- index.php
+++ index.php
@@ -25,13 +25,14 @@
    <div id="slide-accueil">
 	<div id="cbp-fwslider" class="cbp-fwslider">
 	<ul>
-		<li><a href="#"><img src="images/slider_11.png" alt="img01"/></a></li>
-		<li><a href="#"><img src="images/slider_1.png" alt="img01"/></a></li>
-		<li><a href="#"><img src="images/slide3.jpg" alt="img01"/></a></li>
-		<li><a href="#"><img src="images/slide2.jpg" alt="img01"/></a></li>
-		<li><a href="#"><img src="images/slide1.jpg" alt="img01"/></a></li>
-		<li><a href="#"><img src="images/slider-7.png" alt="img01"/></a></li>
-		<li><a href="#"><img src="images/slider_3.png" alt="img01"/></a></li>
+		<li><a href="#"><img src="images/slider_11.png" alt="slide1"/></a></li>
+		<li><a href="#"><img src="images/slider_1.png" alt="slide2"/></a></li>
+		<li><a href="#"><img src="images/slide3.jpg" alt="slide3"/></a></li>
+		<li><a href="#"><img src="images/slide2.jpg" alt="slide4"/></a></li>
+		<li><a href="#"><img src="images/slide1.jpg" alt="slide5"/></a></li>
+		<li><a href="#"><img src="images/slider-7.png" alt="slide6"/></a></li>
+		<li><a href="#"><img src="images/slider_3.png" alt="slide7"/></a></li>
+        <li><a href="#"><img src="images/slider_5.png" alt="slide8"/></a></li>
 	</ul>
     </div>
    </div>
