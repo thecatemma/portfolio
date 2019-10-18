f87e314327c325b64a6c1833d8503c0221b06b84
thecatemma <husson.emmanuelle.miou@gmail.com>
2019-10-18 23:15:53 +0200

    spaces

--- portfolio.php
+++ portfolio.php
@@ -254,19 +254,15 @@ Le product owner est l'unique responsable de l'actualisation du product backlog,
         <div class="photos_serie">
             <img src="images/EBC1.png" width="200" alt="Proposal 1">
         </div>
-
         <div class="photos_serie">
             <img src="images/EBC2.png" width="200" alt="Proposal 2">
         </div>
-
         <div class="photos_serie">
             <img src="images/EBC4.png" width="200" alt="Proposal 3">
         </div>
-
         <div class="photos_serie">
             <img src="images/EBC5.png" width="200" alt="Proposal 4">
-        </div>
-        
+        </div>     
         <div class="photos_serie">
             <img src="images/EBC6.png" width="200" alt="Proposal 4">
         </div>
