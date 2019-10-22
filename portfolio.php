269c284d47831cbab2d232152da6bcdaf2555f3d
thecatemma <husson.emmanuelle.miou@gmail.com>
2019-10-22 23:44:09 +0200

    spaces

--- portfolio.php
+++ portfolio.php
@@ -11,14 +11,14 @@
 
 <div id="conteneur">
 
-<p id="texte_sap">
-Mes dates de contrat de stage étaient du 18 février au 17 août, à Levallois. <br>
-Les missions qui m'ont été confiées: travailler sur le design d'une interface d'un produit nommé Smart Predict. <br>
-c'est une Entreprise de droit européen qui conçoit et vend des logiciels, notamment des sytèmes de gestion et de maintenance, principalement orienté vers des entreprises et des institutions partout sur le globe. <br>
-SAP a débuté en 1972 sous la forme d'une équipe de cinq collègues ayant comme désir d'établir quelque chose de nouveau. Ensemble, ils ont changé le logiciel d'entreprise et réinventé la manière dont les affaires se déroulaient. Aujourd'hui, en tant que leader du marché des logiciels d'application d'entreprise, ils conçoivent des solutions pour alimenter l'innovation, favoriser l'égalité et répartir les opportunités pour les employés et clients, à travers les frontières et les cultures.<br>
-SAP valorise l'esprit d'entreprise, encourage la créativité et construit des relations durables avec ses employés. Une main-d'œuvre diversifiée et inclusive leur permet de rester concurrentiels et offrir des opportunités. Ils transforment les industries, développent l’économie, élevent les sociétés et préservent leur environnement.<br>
-Entreprise leader du logiciel informatique, c'est une structure qui comptabilise environ 2000 collaborateurs sur le site sur lequel je suis, elle offre des services aux industries afin de les aider dans leur activité <br> 
-Valeurs: transparence, sérieux, fiabilité... <br>
+    <p id="texte_sap">
+    Mes dates de contrat de stage étaient du 18 février au 17 août, à Levallois. <br>
+    Les missions qui m'ont été confiées: travailler sur le design d'une interface d'un produit nommé Smart Predict. <br>
+    c'est une Entreprise de droit européen qui conçoit et vend des logiciels, notamment des sytèmes de gestion et de maintenance, principalement orienté vers des entreprises et des institutions partout sur le globe. <br>
+    SAP a débuté en 1972 sous la forme d'une équipe de cinq collègues ayant comme désir d'établir quelque chose de nouveau. Ensemble, ils ont changé le logiciel d'entreprise et réinventé la manière dont les affaires se déroulaient. Aujourd'hui, en tant que leader du marché des logiciels d'application d'entreprise, ils conçoivent des solutions pour alimenter l'innovation, favoriser l'égalité et répartir les opportunités pour les employés et clients, à travers les frontières et les cultures.<br>
+    SAP valorise l'esprit d'entreprise, encourage la créativité et construit des relations durables avec ses employés. Une main-d'œuvre diversifiée et inclusive leur permet de rester concurrentiels et offrir des opportunités. Ils transforment les industries, développent l’économie, élevent les sociétés et préservent leur environnement.<br>
+    Entreprise leader du logiciel informatique, c'est une structure qui comptabilise environ 2000 collaborateurs sur le site sur lequel je suis, elle offre des services aux industries afin de les aider dans leur activité <br> 
+    Valeurs: transparence, sérieux, fiabilité... <br>
     
 <p><a href="#" title="Intro" class="jaime">J&rsquo;aime</a></p>
 	
@@ -26,40 +26,40 @@ Valeurs: transparence, sérieux, fiabilité... <br>
 	
  <h2>Forces</h2>
     
-Nombre de collaborateurs. Belle répartition de tâches. Sait s’adapter aux besoins de ses clients. Logiciels qui permettent l'harmonisation de tous les logiciels dans une structure : avec un ERP SAP il n'y a pas besoin de saisir les données dans plusieurs logiciels, cela permet donc un gain de temps et une minimisation des erreurs humaines. L'ERP SAP est un moyen de renforcer la sécurité pour l'entreprise notamment grâce au paramétrage des droits d'accès. <br>
-<br>
+    Nombre de collaborateurs. Belle répartition de tâches. Sait s’adapter aux besoins de ses clients. Logiciels qui permettent l'harmonisation de tous les logiciels dans une structure : avec un ERP SAP il n'y a pas besoin de saisir les données dans plusieurs logiciels, cela permet donc un gain de temps et une minimisation des erreurs humaines. L'ERP SAP est un moyen de renforcer la sécurité pour l'entreprise notamment grâce au paramétrage des droits d'accès. <br>
+    <br>
 	
 <h2>Opportunités</h2>
     
-Entreprise présente dans le monde entier.
-Leader sur le marché des logiciels d’entreprise (ERP).
-Premier éditeur de logicel de gestion pour les grandes entreprises : SAP réalise 43% de PDM en France grâce à la filiére des grandes entreprises (qui représentent plus de 75% de son chiffre d’affaires).
-Importance des logiciels CRM (Customer Relationship Management).<br><br>
+    Entreprise présente dans le monde entier.
+    Leader sur le marché des logiciels d’entreprise (ERP).
+    Premier éditeur de logicel de gestion pour les grandes entreprises : SAP réalise 43% de PDM en France grâce à la filiére des grandes entreprises (qui représentent plus de 75% de son chiffre d’affaires).
+    Importance des logiciels CRM (Customer Relationship Management).<br><br>
 	
 <h2>Faiblesses</h2>
     
-Les logiciels SAP doivent toujours évoluer pour répondre aux attentes des entreprises.
-Durée d’implantation du logiciel longue : 6 à 12 mois.
-Coûts d'investissement élevés à répartir entre l'implantation du logiciel , la formation des salariés et la maintenance.
-Maintenance évolutive ou corrective : cela coute à l’entreprise. <br><br>
+    Les logiciels SAP doivent toujours évoluer pour répondre aux attentes des entreprises.
+    Durée d’implantation du logiciel longue : 6 à 12 mois.
+    Coûts d'investissement élevés à répartir entre l'implantation du logiciel , la formation des salariés et la maintenance.
+    Maintenance évolutive ou corrective : cela coute à l’entreprise. <br><br>
 
 <h2>Menaces</h2>
     
-Concurrents très présents sur le marché.
-De nouveaux logiciels arrivent sur le marché, comme les logiciels pour les PME entre autres.
-La protection des données. Comme toute entreprise centralisant et utilisant des data, SAP doit contrer la menace du piratage informatique et s’assurer qu’aucune donnée personnelle de ses clients ne soit divulguée.
-Augmentation globale des ERP sur le marché.
-Évolutions permanentes sur le marché des ERP : Cela oblige l'entreprise à faire évoluer ses logiciels et engendre donc des coûts supplémentaires.
-SAP a tardé avant de s'orienter vers le marché des PME.<br>
-<br>
+    Concurrents très présents sur le marché.
+    De nouveaux logiciels arrivent sur le marché, comme les logiciels pour les PME entre autres.
+    La protection des données. Comme toute entreprise centralisant et utilisant des data, SAP doit contrer la menace du piratage informatique et s’assurer qu’aucune donnée personnelle de ses clients ne soit divulguée.
+    Augmentation globale des ERP sur le marché.
+    Évolutions permanentes sur le marché des ERP : Cela oblige l'entreprise à faire évoluer ses logiciels et engendre donc des coûts supplémentaires.
+    SAP a tardé avant de s'orienter vers le marché des PME.<br>
+    <br>
 	
 <h1>MISSION</h1>
     
 <h2>Présentation de votre mission</h2><br>
-Des stakeholders, notamment les Product Owners et Product Experts nous remontaient des problématiques rencontrées par les utilisateurs. Ils nous présentaient des wireframes illustratifs des concepts puis c'était à Cédric et moi de proposer des solutions design en s'appuyant sur les guidelines internes à l'entreprise avec Sketch. Les product owners utilisent UML. <br>
-Pour cela nous avions des réunions afin de discuter sur quoi faire pour la suite, parfois in English.<br>
-Le product owner est l'unique responsable de l'actualisation du product backlog, priorise les user stories formulées dans le product backlog, prend des décisions structurantes à partir du product backlog, surveille le budget et le planning grâce au product backlog, participe à la transparence du projet avec le product backlog. 3 rôles de l'équipe Scrum (product owner, membre de l'équipe de développement et scrum master) des autres parties prenantes du projet. <br>	
-<br>
+    Des stakeholders, notamment les Product Owners et Product Experts nous remontaient des problématiques rencontrées par les utilisateurs. Ils nous présentaient des wireframes illustratifs des concepts puis c'était à Cédric et moi de proposer des solutions design en s'appuyant sur les guidelines internes à l'entreprise avec Sketch. Les product owners utilisent UML. <br>
+    Pour cela nous avions des réunions afin de discuter sur quoi faire pour la suite, parfois in English.<br>
+    Le product owner est l'unique responsable de l'actualisation du product backlog, priorise les user stories formulées dans le product backlog, prend des décisions structurantes à partir du product backlog, surveille le budget et le planning grâce au product backlog, participe à la transparence du projet avec le product backlog. 3 rôles de l'équipe Scrum (product owner, membre de l'équipe de développement et scrum master) des autres parties prenantes du projet. <br>	
+    <br>
     
 <h2>Smart Predict</h2><br>
 
