c5387ce59d3bead72f4b05fcb2355d1edd847fe4
thecatemma <hemasn@icloud.com>
2019-11-03 19:45:00 +0100

    clean footer

--- footer.php
+++ footer.php
@@ -1,43 +1,48 @@
 <footer id="footer">
+    
 	<div id="footer-top">
 	 <ul>
-	  <li><div class="titre-footer">PORT<b>FOLIO</b></div></li>
-         
-	  <li><p>Emmanuelle Husson, est née passionnée par l'Art, la création et le Design. Elle pense que l'humain ne doit créer que s'il arrive à hauteur de la nature en terme d'esthétisme. La nature n'a pas besoin de lui, elle se crée seule. Après exploration en informatique, elle souhaite poursuivre sa réalisation de projets artistiques.
-        <br>"husson" est un nom de famille assez repandu en lorraine et dans l'est, represente une forme affective de hue, nom de personne d'origine germanique, issu de hug qui signifie intelligence, esprit .</p></li>
-         
+          <li><div class="titre-footer">PORT<b>FOLIO</b></div></li>
+
+          <li><p>Emmanuelle Husson, est née passionnée par l'Art, la création et le Design. Elle pense que l'humain ne doit créer que s'il arrive à hauteur de la nature en terme d'esthétisme. La nature n'a pas besoin de lui, elle se crée seule. Après exploration en informatique, elle souhaite poursuivre sa réalisation de projets artistiques.
+            <br>"husson" est un nom de famille assez repandu en lorraine et dans l'est, represente une forme affective de hue, nom de personne d'origine germanique, issu de hug qui signifie intelligence, esprit .</p></li>
+
 	 </ul>
 		
-	 <ul>
-          <li><div class="titre-footer">Hiring<a href="PayPal.Me/Hemasn/" class="rs" title="paypal"><i class="fab fa-buromobelexperte" alt="Paypal"></i></a> </div></li>
+	   <ul>
+          <li><div class="titre-footer">Hiring<a href="" class="rs" title="paypal"><i class="fab fa-buromobelexperte" alt="Paypal"></i></a></div></li>
          
           <li><p>UIUX | Design | Analytic Drawing | Art Direction | Code</p></li>
-    <br>
          
           <li><p>Crête, Grèce, Ecosse, Maroc, Stockhlom, Berlin, Amsterdam, Espagne, Tunisie, Sicile, Malte, Corse, Chypre, Londres, Prague, Vienne, Skopje. . . France</p></li>
          
-	 </ul>	
+	   </ul>	
 		
 		 <ul>
+             
 		  <li><div class="titre-footer">Follow <a href="https://www.linkedin.com/in/emmanuelle-husson-92709b130/" class="rs" title="globe"><i class="fa fa-globe" alt="Globe"></i></a></div></li>
+             
 		   <li id="img_footer">
-			<a href="https://www.linkedin.com/in/emmanuelle-husson-92709b130/"><img src="images/icon-linkedin-footer.png" alt="Facebook"></a>
-			<a href="https://hemasn.tumblr.com/"><img src="images/icon-twitter-footer.png" alt="Twitter"></a>
-			<a href="https://www.youtube.com/channel/UCHQyXitCjlQQrSXQCB-fn0w?"><img src="images/icon-youtube-footer.png" alt="YouTube"></a>
-			<a href="https://www.behance.net/hussonemmad23e"><img src="images/icon-behance-footer.png" alt="Behance"></a>
-			<a><img id="logo_footer" src="images/logo.png" alt="logo"></a>
+                <a href="https://www.linkedin.com/in/emmanuelle-husson-92709b130/"><img src="images/icon-linkedin-footer.png" alt="Facebook"></a>
+                <a href="https://hemasn.tumblr.com/"><img src="images/icon-twitter-footer.png" alt="Twitter"></a>
+                <a href="https://www.youtube.com/channel/UCHQyXitCjlQQrSXQCB-fn0w?"><img src="images/icon-youtube-footer.png" alt="YouTube"></a>
+                <a href="https://www.behance.net/hussonemmad23e"><img src="images/icon-behance-footer.png" alt="Behance"></a>
+                <a><img id="logo_footer" src="images/logo.png" alt="logo"></a>
 		   </li>
+             
 		 </ul>
 
      <div class="clearboth"></div>		
 	</div>
+    
 </footer>
 	
 <footer id="footer2">
+    
 	<div id="footer-bottom">
+        
 		<ul class="copyright">
 			<li>©<b>HEMASN</b> | Digital Designer</li><br>
-			<!--<li>This website is <b>handmade</b></li><br>-->
 		</ul>
 		
 		<ul class="liens">
@@ -47,7 +52,9 @@
 			<li><a href="contact.php">Contact</a></li>
 			<!--<li><a href="#">Mentions légales</a></li>-->
 		</ul>
+        
 	</div>
+    
 </footer>
 
 </body>
