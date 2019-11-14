9f6f877350de28d93b1745bbe407bd6a7d6cdf68
thecatemma <hemasn@icloud.com>
2019-11-14 10:15:16 +0100

    clean

--- portfolio.php
+++ portfolio.php
@@ -34,39 +34,47 @@
 	
  <h2>Forces</h2>
     
-    <p>Nombre de collaborateurs. Belle répartition de tâches. Sait s’adapter aux besoins de ses clients. Logiciels qui permettent l'harmonisation de tous les logiciels dans une structure : avec un ERP SAP il n'y a pas besoin de saisir les données dans plusieurs logiciels, cela permet donc un gain de temps et une minimisation des erreurs humaines. L'ERP SAP est un moyen de renforcer la sécurité pour l'entreprise notamment grâce au paramétrage des droits d'accès.</p>
+    <p>
+    Nombre de collaborateurs. Belle répartition de tâches. Sait s’adapter aux besoins de ses clients. Logiciels qui permettent l'harmonisation de tous les logiciels dans une structure : avec un ERP SAP il n'y a pas besoin de saisir les données dans plusieurs logiciels, cela permet donc un gain de temps et une minimisation des erreurs humaines. L'ERP SAP est un moyen de renforcer la sécurité pour l'entreprise notamment grâce au paramétrage des droits d'accès.
+    </p>
     
 <br>
 <br>
 	
 <h2>Opportunités</h2>
     
-    <p>Entreprise présente dans le monde entier.
+    <p>
+    Entreprise présente dans le monde entier.
     Leader sur le marché des logiciels d’entreprise (ERP).
     Premier éditeur de logicel de gestion pour les grandes entreprises : SAP réalise 43% de PDM en France grâce à la filière des grandes entreprises (qui représentent plus de 75% de son chiffre d’affaires).
-    Importance des logiciels CRM (Customer Relationship Management).</p>
+    Importance des logiciels CRM (Customer Relationship Management).
+    </p>
     
 <br>
 <br>
 	
 <h2>Faiblesses</h2>
     
-    <p>Les logiciels SAP doivent toujours évoluer pour répondre aux attentes des entreprises.
+    <p>
+    Les logiciels SAP doivent toujours évoluer pour répondre aux attentes des entreprises.
     Durée d’implantation du logiciel longue : 6 à 12 mois.
     Coûts d'investissement élevés à répartir entre l'implantation du logiciel , la formation des salariés et la maintenance.
-    Maintenance évolutive ou corrective : cela coute à l’entreprise.</p>
+    Maintenance évolutive ou corrective : cela coute à l’entreprise.
+    </p>
     
 <br>
 <br>
 
 <h2>Menaces</h2>
     
-    <p>Concurrents très présents sur le marché.
+    <p>
+    Concurrents très présents sur le marché.
     De nouveaux logiciels arrivent sur le marché, comme les logiciels pour les PME entre autres.
     La protection des données. Comme toute entreprise centralisant et utilisant des data, SAP doit contrer la menace du piratage informatique et s’assurer qu’aucune donnée personnelle de ses clients ne soit divulguée.
     Augmentation globale des ERP sur le marché. 
     Évolutions permanentes sur le marché des ERP : Cela oblige l'entreprise à faire évoluer ses logiciels et engendre donc des coûts supplémentaires.
-    SAP a tardé avant de s'orienter vers le marché des PME.</p>
+    SAP a tardé avant de s'orienter vers le marché des PME.
+    </p>
     
 <br>
 <br>
@@ -129,79 +137,81 @@
         
     </div>
 	
-<h2>Participation</h2><br>	
-	
-<div id="contener">
+<h2>Participation</h2>
     
-    <div class="photos_serie">
-        <h3>Assia's drawing</h3>
-            <img src="images/sap_1.png" class="serie" height="250" alt="Dessin Assia équipe">
-    </div>
+<br>	
+	
+    <div id="contener">
 
-    <div class="photos_serie">
-        <h3>Hiérarchie</h3>
-        <img src="images/sap-10.png" class="serie" height="250" alt="Organigramme">
+        <div class="photos_serie">
+            <h3>Assia's drawing</h3>
+                <img src="images/sap_1.png" class="serie" height="250" alt="Dessin Assia équipe">
         </div>
 
-    <div class="photos_serie">
-        <h3>Show & Tell</h3>
-            <img src="images/sap-1.png" class="serie" height="250" alt="Dessin Show and Tell">
-    </div>
+        <div class="photos_serie">
+            <h3>Hiérarchie</h3>
+            <img src="images/sap-10.png" class="serie" height="250" alt="Organigramme">
+            </div>
 
-    <div class="photos_serie">
-        <h3>Les déterminés</h3>
-            <img src="images/sap11.png" class="serie" height="250" alt="Association les Déterminés">
-    </div>
+        <div class="photos_serie">
+            <h3>Show & Tell</h3>
+                <img src="images/sap-1.png" class="serie" height="250" alt="Dessin Show and Tell">
+        </div>
 
-    <div class="photos_serie">
-        <h3>Show & Tell</h3>
-            <img src="images/sap_11.png" class="serie" height="250" alt="Dessin Show and Tell">
-    </div>
+        <div class="photos_serie">
+            <h3>Les déterminés</h3>
+                <img src="images/sap11.png" class="serie" height="250" alt="Association les Déterminés">
+        </div>
 
-    <div class="photos_serie">
-        <h3>SpringBreak</h3>
-            <img src="images/sap_9.png" class="serie" height="250" alt="Concert Petit Bain">
-    </div>
+        <div class="photos_serie">
+            <h3>Show & Tell</h3>
+                <img src="images/sap_11.png" class="serie" height="250" alt="Dessin Show and Tell">
+        </div>
 
-    <div class="photos_serie">
-        <h3>On boards</h3>
-            <img src="images/sap_13.png" class="serie" height="250" alt="Partie de réflexion sur les murs">
-    </div>
+        <div class="photos_serie">
+            <h3>SpringBreak</h3>
+                <img src="images/sap_9.png" class="serie" height="250" alt="Concert Petit Bain">
+        </div>
 
-    <div class="photos_serie">
-        <h3>Conference View</h3>
-            <img src="images/sap_14.png" class="serie" height="250" alt="View vue">
-    </div>
+        <div class="photos_serie">
+            <h3>On boards</h3>
+                <img src="images/sap_13.png" class="serie" height="250" alt="Partie de réflexion sur les murs">
+        </div>
 
-    <div class="photos_serie">
-        <h3>Meeting Room</h3>
-            <img src="images/sap_15.png" class="serie" height="250" alt="Salle de réunion">
-    </div>
+        <div class="photos_serie">
+            <h3>Conference View</h3>
+                <img src="images/sap_14.png" class="serie" height="250" alt="View vue">
+        </div>
 
-    <div class="photos_serie">
-        <h3>Event</h3>
-            <img src="images/sap_16.png" class="serie" height="250" alt="Evènement Paris Event">
-    </div>
+        <div class="photos_serie">
+            <h3>Meeting Room</h3>
+                <img src="images/sap_15.png" class="serie" height="250" alt="Salle de réunion">
+        </div>
 
-    <div class="photos_serie">
-        <h3>Working Conditions</h3>
-            <img src="images/sap_17.png" class="serie" height="250" alt="Bureau Desk">
-    </div>
+        <div class="photos_serie">
+            <h3>Event</h3>
+                <img src="images/sap_16.png" class="serie" height="250" alt="Evènement Paris Event">
+        </div>
 
-    <div class="photos_serie">
-        <h3>Office View</h3>
-            <img src="images/sap_19.png" class="serie" height="250" alt="Vue Tour">
+        <div class="photos_serie">
+            <h3>Working Conditions</h3>
+                <img src="images/sap_17.png" class="serie" height="250" alt="Bureau Desk">
+        </div>
+
+        <div class="photos_serie">
+            <h3>Office View</h3>
+                <img src="images/sap_19.png" class="serie" height="250" alt="Vue Tour">
+        </div>
+
     </div>
-    
-</div>
 	
-<h2>Observation</h2><br>
-    
-<h3>Above my Desk</h3>
-    
-     <div class="img">
-         <img src="images/Avatar.png" height="300" alt="Avatar Emmanuelle">
-     </div>
+    <h2>Observation</h2><br>
+
+        <h3>Above my Desk</h3>
+
+             <div class="img">
+                 <img src="images/Avatar.png" height="300" alt="Avatar Emmanuelle">
+             </div>
     
 <br>	
 <br>
@@ -236,61 +246,61 @@
 <br>
 <br>
    
- <section id="client-acc">
-     
-		<div class="slider5">
-			<div class="slide"><img src="images/picto-1.png"></div>
-  			<div class="slide"><img src="images/picto-2.png"></div>
-            <div class="slide"><img src="images/picto-3.png"></div>
-            <div class="slide"><img src="images/picto-4.png"></div>
-            <div class="slide"><img src="images/picto-5.png"></div>
-            <div class="slide"><img src="images/picto-6.png"></div>
-		</div>
-		
-        <script>
-            $(document).ready(function(){
-              $('.slider5').bxSlider({
-                slideWidth: 300,
-                minSlides: 1,
-                maxSlides: 4,
-                moveSlides: 4,
-                slideMargin: 10,
-                preloadImages : 'all',
-                auto:true,
-              });
-            });
-        </script>
-     
- </section>	
+     <section id="client-acc">
+
+            <div class="slider5">
+                <div class="slide"><img src="images/picto-1.png"></div>
+                <div class="slide"><img src="images/picto-2.png"></div>
+                <div class="slide"><img src="images/picto-3.png"></div>
+                <div class="slide"><img src="images/picto-4.png"></div>
+                <div class="slide"><img src="images/picto-5.png"></div>
+                <div class="slide"><img src="images/picto-6.png"></div>
+            </div>
+
+            <script>
+                $(document).ready(function(){
+                  $('.slider5').bxSlider({
+                    slideWidth: 300,
+                    minSlides: 1,
+                    maxSlides: 4,
+                    moveSlides: 4,
+                    slideMargin: 10,
+                    preloadImages : 'all',
+                    auto:true,
+                  });
+                });
+            </script>
+
+     </section>	
     
 <br>
 <br>
    
-<h2>Sketchings EBC</h2>
-      
-    <div id="contener">
-        
-        <div class="photos_serie">
-            <img src="images/EBC1.png" width="200" alt="Proposal 1">
+    <h2>Sketchings EBC</h2>
+
+        <div id="contener">
+
+            <div class="photos_serie">
+                <img src="images/EBC1.png" width="200" alt="Proposal 1">
+            </div>
+
+            <div class="photos_serie">
+                <img src="images/EBC2.png" width="200" alt="Proposal 2">
+            </div>
+
+            <div class="photos_serie">
+                <img src="images/EBC4.png" width="200" alt="Proposal 3">
+            </div>
+
+            <div class="photos_serie">
+                <img src="images/EBC5.png" width="200" alt="Proposal 4">
+            </div>
+
+            <div class="photos_serie">
+                <img src="images/EBC6.png" width="200" alt="Proposal 4">
+            </div>
+
         </div>
-        
-        <div class="photos_serie">
-            <img src="images/EBC2.png" width="200" alt="Proposal 2">
-        </div>
-        
-        <div class="photos_serie">
-            <img src="images/EBC4.png" width="200" alt="Proposal 3">
-        </div>
-        
-        <div class="photos_serie">
-            <img src="images/EBC5.png" width="200" alt="Proposal 4">
-        </div>
-        
-        <div class="photos_serie">
-            <img src="images/EBC6.png" width="200" alt="Proposal 4">
-        </div>
-        
-    </div>
     
 <br>
 <br>
