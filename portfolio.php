5b026b39b1fe7638527ad3d484b6f12956fbe2d2
thecatemma <hemasn@icloud.com>
2020-01-10 14:12:31 +0100

    7 - Join

--- portfolio.php
+++ portfolio.php
@@ -36,9 +36,7 @@
         </div>
 
         <p>Nombre de collaborateurs. Belle répartition de tâches. Sait s’adapter aux besoins de ses clients. Logiciels qui permettent l'harmonisation de tous les logiciels dans une structure : avec un ERP SAP il n'y a pas besoin de saisir les données dans plusieurs logiciels, cela permet donc un gain de temps et une minimisation des erreurs humaines. L'ERP SAP est un moyen de renforcer la sécurité pour l'entreprise notamment grâce au paramétrage des droits d'accès.</p>
-
 <br>
-
         <div class="first-part">
             <div class="title-section">
                 <h2>Opportunités</h2>
@@ -49,9 +47,7 @@
         <p>Entreprise présente dans le monde entier.
         Leader sur le marché des logiciels d’entreprise (ERP).
         Premier éditeur de logicel de gestion pour les grandes entreprises : SAP réalise 43% de PDM en France grâce à la filière des grandes entreprises (qui représentent plus de 75% de son chiffre d’affaires).Importance des logiciels CRM (Customer Relationship Management).</p>
-    
 <br>
-
         <div class="first-part">
             <div class="title-section">
                 <h2>Faiblesses</h2>
@@ -63,9 +59,7 @@
         Durée d’implantation du logiciel longue : 6 à 12 mois.
         Coûts d'investissement élevés à répartir entre l'implantation du logiciel , la formation des salariés et la maintenance.
         Maintenance évolutive ou corrective : cela coute à l’entreprise.</p>
-
 <br>
-    
         <div class="first-part">
             <div class="title-section">
                 <h2>Menaces</h2>
@@ -79,7 +73,6 @@
         Augmentation globale des ERP sur le marché. 
         Évolutions permanentes sur le marché des ERP : Cela oblige l'entreprise à faire évoluer ses logiciels et engendre donc des coûts supplémentaires.
         SAP a tardé avant de s'orienter vers le marché des PME.</p>
-    
 <br>
 	
  <h1>MISION</h1>
@@ -93,10 +86,9 @@
               </div>
           </div>
 
-    <p>Des stakeholders, notamment les Product Owners et Product Experts remontaient des problématiques rencontrées par les utilisateurs. Ils présentaient des wireframes illustratifs des concepts puis c'était aux Designers de proposer des solutions en s'appuyant sur les guidelines internes de l'entreprise, avec comme outil, Sketch. Les product owners utilisent UML.  
-    Pour cela nous avions des réunions afin de discuter sur que faire pour la suite, parfois in English.   
-    Le product owner est l'unique responsable de l'actualisation du product backlog, priorise les user stories formulées dans le product backlog, prend des décisions structurantes à partir du product backlog, surveille le budget et le planning grâce au product backlog, participe à la transparence du projet avec le product backlog. 3 rôles de l'équipe Scrum (product owner, membre de l'équipe de développement et scrum master) des autres parties prenantes du projet.</p>	
-            
+        <p>Des stakeholders, notamment les Product Owners et Product Experts remontaient des problématiques rencontrées par les utilisateurs. Ils présentaient des wireframes illustratifs des concepts puis c'était aux Designers de proposer des solutions en s'appuyant sur les guidelines internes de l'entreprise, avec comme outil, Sketch. Les product owners utilisent UML.  
+        Pour cela nous avions des réunions afin de discuter sur que faire pour la suite, parfois in English.   
+        Le product owner est l'unique responsable de l'actualisation du product backlog, priorise les user stories formulées dans le product backlog, prend des décisions structurantes à partir du product backlog, surveille le budget et le planning grâce au product backlog, participe à la transparence du projet avec le product backlog. 3 rôles de l'équipe Scrum (product owner, membre de l'équipe de développement et scrum master) des autres parties prenantes du projet.</p>	      
 <br>
         <div class="first-part">
             <div class="title-section">
@@ -107,23 +99,17 @@
         </div>
         
         <div id="contener">
-        
             <div class="img">
                 <img src="images/sap9.png" height="175" alt="Profile">
             </div>
-
             <div class="img">
                 <img src="images/sap1.png" height="175" alt="Profile">
             </div>
-
             <div class="img">
                 <img src="images/sap4.png" height="175" alt="Profile">
             </div>
-    
         </div>
-
-<br>
-            
+<br>     
     <section>
           <div class="first-part">
               <div class="title-section">
@@ -146,9 +132,7 @@
                 <img src="images/sap8.png" width="300" alt="Proposal 3">
             </div>
         </div>
-        
 <br>
-        
           <div class="first-part">
               <div class="title-section">
                   <h2>Participation</h2>
@@ -158,71 +142,56 @@
           </div>
         
         <div id="contener">
-
             <div class="photos_serie">
                 <h3>Assia's drawing</h3>
                     <img src="images/sap_1.png" class="serie" height="250" alt="Dessin Assia équipe">
             </div>
-
             <div class="photos_serie">
                 <h3>Hiérarchie</h3>
-                <img src="images/sap-10.png" class="serie" height="250" alt="Organigramme">
-                </div>
-
+                <img src="images/sap-10.png" class="serie" height="250" alt="Organigramme"> 
+            </div>
             <div class="photos_serie">
                 <h3>Show & Tell</h3>
                     <img src="images/sap-1.png" class="serie" height="250" alt="Dessin Show and Tell">
             </div>
-
             <div class="photos_serie">
                 <h3>Les déterminés</h3>
                     <img src="images/sap11.png" class="serie" height="250" alt="Association les Déterminés">
             </div>
-
             <div class="photos_serie">
                 <h3>Show & Tell</h3>
                     <img src="images/sap_11.png" class="serie" height="250" alt="Dessin Show and Tell">
             </div>
-
             <div class="photos_serie">
                 <h3>SpringBreak</h3>
                     <img src="images/sap_9.png" class="serie" height="250" alt="Concert Petit Bain">
             </div>
-
             <div class="photos_serie">
                 <h3>On boards</h3>
                     <img src="images/sap_13.png" class="serie" height="250" alt="Partie de réflexion sur les murs">
             </div>
-
             <div class="photos_serie">
                 <h3>Conference View</h3>
                     <img src="images/sap_14.png" class="serie" height="250" alt="View vue">
             </div>
-
             <div class="photos_serie">
                 <h3>Meeting Room</h3>
                     <img src="images/sap_15.png" class="serie" height="250" alt="Salle de réunion">
             </div>
-
             <div class="photos_serie">
                 <h3>Event</h3>
                     <img src="images/sap_16.png" class="serie" height="250" alt="Evènement Paris Event">
             </div>
-
             <div class="photos_serie">
                 <h3>Working Conditions</h3>
                     <img src="images/sap_17.png" class="serie" height="250" alt="Bureau Desk">
             </div>
-
             <div class="photos_serie">
                 <h3>Office View</h3>
                     <img src="images/sap_19.png" class="serie" height="250" alt="Vue Tour">
             </div>
-
-        </div>
-        
+        </div>  
 <br>
-        
         <div class="first-part">
             <div class="title-section">
                 <h2>Observation</h2>
@@ -230,13 +199,10 @@
                 <p>Above my desk</p>
             </div>
         </div>
-
              <div class="img">
                  <img src="images/Avatar.png" height="300" alt="Avatar Emmanuelle">
              </div>
-        
 <br>
-        
         <div class="first-part">
             <div class="title-section">
                 <h2>Data Warehouse Cloud</h2>
@@ -275,8 +241,7 @@
                 <div class="slide"><img src="images/picto-3.png"></div>
                 <div class="slide"><img src="images/picto-4.png"></div>
                 <div class="slide"><img src="images/picto-5.png"></div>
-                <div class="slide"><img src="images/picto-6.png"></div>
-                
+                <div class="slide"><img src="images/picto-6.png"></div> 
             </div>
             <script> 
                 $(document).ready(function(){
